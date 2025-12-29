74a5eafccd59b008d628289f287ef9244d16c9a6
hamzaou <hamzaouazzi97@gmail.com>
2025-12-29 12:46:56 +0100

    :fire: remove temporary method

--- src/main/java/com/ecommerce/testprojetecommerce/repository/CategorieRepository.java
+++ src/main/java/com/ecommerce/testprojetecommerce/repository/CategorieRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-    List<Categorie> findByParentIsNull();
 }
