bd108090f26c4fb76e3570b048fe8052bb8e8324
hamzaou <hamzaouazzi97@gmail.com>
2025-12-29 10:21:29 +0100

    first commit

---
+++ src/main/java/com/ecommerce/testprojetecommerce/TestProjetECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.testprojetecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjetECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjetECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/testprojetecommerce/TestProjetECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.testprojetecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjetECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
